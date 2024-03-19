1cb40ed3284df6a4a6e6aa6ed4a26d04be8e9f2e
Arooran-Sivakarunai <arooransivakarunai@gmail.com>
2024-03-19 00:04:07 -0400

    changed app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
+#test
 @app.route("/")
 def hello_world():
-    return render_template("make_bug.html")
+    return render_template("log_in.html")
 
